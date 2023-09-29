e83a7473bce11cdad396bce33b3367af8e262020
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-09-29 22:59:24 +0530

    Revert "Youtube Clone Skeleton Done"
    This reverts commit 7af4c63461f1ef4c96bdd9ab5d02123e3804910a.

--- src/Components/ButtonList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ButtonList = () => {
-  return (
-    <div>ButtonList</div>
-  )
-}
-
-export default ButtonList
--- src/Components/VideoContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VideoContainer = () => {
-  return (
-    <div>VideoContainer</div>
-  )
-}
-
-export default VideoContainer
