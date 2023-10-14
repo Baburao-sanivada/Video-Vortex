4f94e842e81e2a2ab8e4c5d6e04d914683d97140
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-10-14 22:45:35 +0530

    search

--- src/Components/SearchResultsPage.js
+++ src/Components/SearchResultsPage.js
@@ -35,6 +35,9 @@ const SearchResultsPage = () => {
           dispatcher(setChannelId(result?.snippet?.channelId))
           console.log("channel Id ",result?.snippet?.channelId);
         }}>
+
+
+          
         <SearchVideoCard 
         data={result?.snippet}/>
         </Link>
