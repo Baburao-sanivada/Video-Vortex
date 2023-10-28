edf0a83dbcd0151f835e51b2b7e59488300d8363
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-10-28 21:26:35 +0530

    Removed User_ICon

--- src/Components/CommentsContainer.js
+++ src/Components/CommentsContainer.js
@@ -1,11 +1,11 @@
-import { user_icon } from "../utils/paths";
 import { comments } from "../utils/MockCommentsData";
+import { BiUserCircle } from "react-icons/bi";
 
 
 const CommentItem=({data})=>{
     const {name,text,replies}=data;
     return <div className="flex p-2 m-2 bg-gray-50 dark:bg-slate-800 dark:text-white">
-        <img className="h-12" alt="user-icon" src={user_icon}/>
+        <BiUserCircle className=" dark:bg-slate-800 text-2xl" alt="user-Icon" />
         <div className="px-2">
             <p>{name}</p>
             <p>{text}</p>
