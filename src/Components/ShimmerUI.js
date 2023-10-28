305df0fd5358daa5409b0ee98132f2ae4529931a
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-10-28 20:56:34 +0530

    Worked on Dark Mode Shimmer UI

--- src/Components/ShimmerUI.js
+++ src/Components/ShimmerUI.js
@@ -3,50 +3,50 @@ import React from 'react'
 const ShimmerUI = () => {
   return (
     <div className='flex flex-wrap'>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 '>
-            <div className='w-50 h-[150px] bg-gray-200 '></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 '>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600' >
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
-        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100'>
-            <div className='w-50 h-[150px] bg-gray-200'></div>
+        <div className='m-2 w-60 h-[300px] shadow-lg bg-gray-100 dark:bg-slate-600'>
+            <div className='w-50 h-[150px] bg-gray-200 dark:bg-slate-700 rounded-lg'></div>
         </div>
         
     </div>
