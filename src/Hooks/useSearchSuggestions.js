48fe1bae268023a3533aa7317c55c29a34f4beea
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-10-29 14:28:00 +0530

    Updated Search suggestions api

---
+++ src/Hooks/useSearchSuggestions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useSearchSuggestions = (QueryText) => {
+  return (
+    <div>useSearchSuggestions</div>
+  )
+}
+
+export default useSearchSuggestions
--- src/utils/paths.js
+++ src/utils/paths.js
@@ -7,7 +7,7 @@ export const videoDetailsApi="https://youtube.googleapis.com/youtube/v3/videos?p
 
 export const youtube_video_api="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+Google_api_key
 
-export const youtube_search_api="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const youtube_search_api="https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const Search_results_api="https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="
 
