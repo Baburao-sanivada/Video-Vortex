2ac986c568e305f6c2a961109bbf247b3f9f6459
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-01 11:05:09 +0530

    Worked on Button List All

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({name}) => {
   return (
     <div>
-        <button className="px-3 py-1 my-2 mx-1 items-center border border-gray-100 rounded-lg bg-gray-100 hover:bg-gray-200 dark:bg-slate-700 dark:text-white dark:border-none dark:hover:bg-slate-600">{name}</button>
+        <button className={`px-3 py-1 my-2 mx-1 items-center border border-gray-100 rounded-lg   ${name=="All"?"bg-black text-white dark:bg-white dark:text-slate-800":'bg-gray-100 dark:bg-slate-700 dark:text-white dark:hover:bg-slate-600 hover:bg-gray-200'} dark:border-none `}>{name}</button>
     </div>
   )
 }
