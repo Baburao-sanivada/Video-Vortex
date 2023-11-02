f649c0fc873a2dd5c8679d015e050ce4b4a83e09
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-02 22:36:28 +0530

    Updated API Key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,38 +1,53 @@
+export const Google_api_key = "AIzaSyDPWlLUuH28EP5tKFe4p04uRCB4m9GVO2Q";
 
-export const Google_api_key="AIzaSyA7Zn_fzkXnZvsZ4b9R67erY-yeUMO3vVY"
+export const video_recommendations_api = `https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&maxResults=50&regionCode=in&key=${Google_api_key}&channelId=`;
 
-export const video_recommendations_api=`https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&maxResults=50&regionCode=in&key=${Google_api_key}&channelId=`
+export const videoDetailsApi =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  Google_api_key;
 
-export const videoDetailsApi="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key="+Google_api_key;
+export const youtube_video_api =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  Google_api_key;
 
-export const youtube_video_api="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+Google_api_key
+export const youtube_search_api =
+  "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const youtube_search_api="https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const Search_results_api =
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=";
 
-export const Search_results_api="https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="
+export const channelImage_api =
+  "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  Google_api_key;
 
-export const channelImage_api="https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key="+Google_api_key
-
-export const video_comments_details_api="https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=100&order=relevance&key="+Google_api_key+"&videoId="
+export const video_comments_details_api =
+  "https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=100&order=relevance&key=" +
+  Google_api_key +
+  "&videoId=";
 var nameList = [
-    'Adil', 'Babu', 'Afroz', 'Kiran',
-    'Vinay', 'Balu', 'Hk', 'Sai Teja'
-  ];
+  "Adil",
+  "Babu",
+  "Afroz",
+  "Kiran",
+  "Vinay",
+  "Balu",
+  "Hk",
+  "Sai Teja",
+];
 export function generateRandomName() {
-        return nameList[Math.floor(Math.random() * nameList.length)];
-      };
+  return nameList[Math.floor(Math.random() * nameList.length)];
+}
 
-      // Generating Random Text for Live Chat
+// Generating Random Text for Live Chat
 export function getRandomText(length) {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        let counter = 0;
-        while (counter < length) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-          counter += 1;
-        }
-        return result;
-    }
-
-
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
