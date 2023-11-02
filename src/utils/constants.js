0bac13baef2fecfc7d5c2298e8f6833dc31c32e0
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-11-02 11:26:07 +0530

    Changed Api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 
-export const Google_api_key="AIzaSyCAX7aVBjRq-_byIclKfbRJO_5y36IT7dA"
+export const Google_api_key="AIzaSyCbzwMhy-9hGb-dFL5u2urQ7z6Wr1s8O-I"
 
 export const video_recommendations_api=`https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&maxResults=50&regionCode=in&key=${Google_api_key}&channelId=`
 
